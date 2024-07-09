d11f1780ff0c1b4c8234a50a3c50d6991dd18c10
Wystov <wystov@gmail.com>
2024-07-09 20:10:21 +0200

    feat: throw if no api key

--- src/api/movie-db.ts
+++ src/api/movie-db.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 const axiosMovieApi = axios.create();
 
 const apiKey = import.meta.env.VITE_MOVIEDB_API_KEY;
+
+if (!apiKey) throw new Error('No API key found. You can get one at https://api.themoviedb.org');
+
 const baseUrl = 'https://api.themoviedb.org/3/';
 
 axiosMovieApi.interceptors.request.use((config) => {
