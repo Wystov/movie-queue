eb730af9a2c88773b8ddf5ed19a41ac7b2063188
Wystov <wystov@gmail.com>
2024-07-15 02:29:56 +0200

    feat: show scrollToTop after scrolling down

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <VApp>
     <AppSideBar />
-    <VMain>
+    <VMain v-scroll="onScroll">
       <router-view />
     </VMain>
-    <scroll-to-top />
+    <scroll-to-top :show="showScrollToTop" />
   </VApp>
 </template>
 
 <script setup lang="ts">
 import ScrollToTop from '@/components/ScrollToTop.vue';
+import { ref } from 'vue';
 import AppSideBar from './components/SideBar.vue';
 import { useQueueStore } from './stores/queue';
 import { useSearchStore } from './stores/search';
@@ -18,6 +19,12 @@ const queue = useQueueStore();
 const { getMovieList } = useSearchStore();
 
 getMovieList();
+
+const showScrollToTop = ref(false);
+
+const onScroll = () => {
+  showScrollToTop.value = window.scrollY > 500;
+};
 
 queue.$subscribe((_, state) => {
   localStorage.setItem('movie-queue: queue', JSON.stringify(state.movies));
